1a1644f15d4db3cf9988f2fb2639e6091cbd0616
lifakpo <lifakpo@yahoo.fr>
2018-04-09 08:54:03 +0000

    ajout toString bloc

--- Developpement/Composant1/Composant1/FileInterface.cpp
+++ Developpement/Composant1/Composant1/FileInterface.cpp
@@ -67,50 +67,51 @@ void FileInterface::insert(Bloc bloc) {
 }
 
 string FileInterface::toString(bool allInfos, string hash) {
-	Bloc b = findByHash(hash);
-	string str ="{";
-	if (allInfos == true) {
-		str += "\n\t\"hash\": \"" + string(b.hash);
-		str += "\",\n\t\"nonce\": \"" + to_string(b.nonce) + "\",";
-	} 
-	str += "\n\t\"previous_hash\": \"" + string(b.previous_hash);
-	str += "\",\n\t\"num\": \"" + to_string(b.num);
+	Bloc bloc = findByHash(hash);
+	return bloc.ToString();
+}
+
+string Bloc::ToString() {
+	string str = "{";
+	str += "\n\t\"hash\": \"" + string(hash);
+	str += "\",\n\t\"nonce\": \"" + to_string(nonce) + "\",";
+	str += "\n\t\"previous_hash\": \"" + string(previous_hash);
+	str += "\",\n\t\"num\": \"" + to_string(num);
 	str += "\",\n\t\"TX\": { \n\t\t";
 	str += "\"TXI\": [{";
 
-	for (unsigned int i = 0; i < b.tx1.TXIs.size(); i++) {
-		str += "\"nBloc\": \"" + to_string(b.tx1.TXIs[i].nBloc);
-		str += "\",\"nUTXO\": \"" + to_string(b.tx1.TXIs[i].nUtxo);
-		str += "\",\"nTx\": \"" + to_string(b.tx1.TXIs[i].nTx);
-		str += "\",\"signature\": \"" + string(b.tx1.TXIs[i].signature, b.tx1.TXIs[i].signature + sizeof b.tx1.TXIs[i].signature / sizeof b.tx1.TXIs[i].signature[0]);
+	for (unsigned int i = 0; i < tx1.TXIs.size(); i++) {
+		str += "\"nBloc\": \"" + to_string(tx1.TXIs[i].nBloc);
+		str += "\",\"nUTXO\": \"" + to_string(tx1.TXIs[i].nUtxo);
+		str += "\",\"nTx\": \"" + to_string(tx1.TXIs[i].nTx);
+		str += "\",\"signature\": \"" + string(tx1.TXIs[i].signature, tx1.TXIs[i].signature + sizeof tx1.TXIs[i].signature / sizeof tx1.TXIs[i].signature[0]);
 		str += "\"}";
-		if (i != (b.tx1.TXIs.size() - 1)) {
+		if (i != (tx1.TXIs.size() - 1)) {
 			str += ",";
 		}
 	}
 	str += "],\n";
 	str += "\"UTXO\": [{";
-	for (unsigned int i = 0; i < b.tx1.UTXOs.size(); i++) {
-		str += "\"nBloc\": \"" + to_string(b.tx1.UTXOs[i].nBloc);
-		str += "\",\"nUTXO\": \"" + to_string(b.tx1.UTXOs[i].nUTX0);
-		str += "\",\"nTx\": \"" + to_string(b.tx1.UTXOs[i].nTx);
-		str += "\",\"montant\": \"" + to_string(b.tx1.UTXOs[i].montant);
-		str += "\",\"cle_publique\": \"" + string(b.tx1.UTXOs[i].dest, b.tx1.UTXOs[i].dest + sizeof b.tx1.UTXOs[i].dest / sizeof b.tx1.UTXOs[i].dest[0]);
-		str += "\",\"hash\": \"" + string(b.tx1.UTXOs[i].hash, b.tx1.UTXOs[i].hash + sizeof b.tx1.UTXOs[i].hash / sizeof b.tx1.UTXOs[i].hash[0]);
+	for (unsigned int i = 0; i < tx1.UTXOs.size(); i++) {
+		str += "\"nBloc\": \"" + to_string(tx1.UTXOs[i].nBloc);
+		str += "\",\"nUTXO\": \"" + to_string(tx1.UTXOs[i].nUTX0);
+		str += "\",\"nTx\": \"" + to_string(tx1.UTXOs[i].nTx);
+		str += "\",\"montant\": \"" + to_string(tx1.UTXOs[i].montant);
+		str += "\",\"cle_publique\": \"" + string(tx1.UTXOs[i].dest, tx1.UTXOs[i].dest + sizeof tx1.UTXOs[i].dest / sizeof tx1.UTXOs[i].dest[0]);
+		str += "\",\"hash\": \"" + string(tx1.UTXOs[i].hash, tx1.UTXOs[i].hash + sizeof tx1.UTXOs[i].hash / sizeof tx1.UTXOs[i].hash[0]);
 		str += "\"}";
-		if (i != (b.tx1.UTXOs.size() - 1)) {
+		if (i != (tx1.UTXOs.size() - 1)) {
 			str += ",";
 		}
 	}
 
 	str += "]\n},";
 	str += "\",\n\t\"TXM\": {";
-	str += "\n\t\t\"nBloc\": \"" + to_string(b.tx0.utxo[0].nBloc);
-	str += "\",\n\t\"nUTXO\": \"" + to_string(b.tx0.utxo[0].nUTX0) + "\",";
-	str += "\",\n\t\"montant\": \"" + to_string(b.tx0.utxo[0].montant) + "\",";
-	str += "\",\n\t\"cle_publique\": \"" + string(b.tx0.utxo[0].dest, b.tx0.utxo[0].dest + sizeof b.tx0.utxo[0].dest / sizeof b.tx0.utxo[0].dest[0]) + "\",";
-	str += "\",\n\t\"hash\": \"" + string(b.tx0.utxo[0].hash, b.tx0.utxo[0].hash + sizeof b.tx0.utxo[0].hash / b.tx0.utxo[0].hash[0]) + "\"\n\t}\n}";
-
+	str += "\n\t\t\"nBloc\": \"" + to_string(tx0.utxo[0].nBloc);
+	str += "\",\n\t\"nUTXO\": \"" + to_string(tx0.utxo[0].nUTX0) + "\",";
+	str += "\",\n\t\"montant\": \"" + to_string(tx0.utxo[0].montant) + "\",";
+	str += "\",\n\t\"cle_publique\": \"" + string(tx0.utxo[0].dest, tx0.utxo[0].dest + sizeof tx0.utxo[0].dest / sizeof tx0.utxo[0].dest[0]) + "\",";
+	str += "\",\n\t\"hash\": \"" + string(tx0.utxo[0].hash, tx0.utxo[0].hash + sizeof tx0.utxo[0].hash / sizeof tx0.utxo[0].hash[0]) + "\"\n\t}\n}";
 	return str;
 }
 
